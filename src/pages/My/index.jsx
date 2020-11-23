93fc057bc02279a058653cf97d357726c37ca6b2
wangcx1229 <17638166179@163.com>
2020-11-23 14:46:40 +0800

    first

---
+++ src/pages/My/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import FootNav from '../../components/FootNav'
+
+ export default class My extends React.Component{
+     render(){
+         return(
+             <div>
+                 <FootNav/>
+             </div>
+         )
+     }
+ }
